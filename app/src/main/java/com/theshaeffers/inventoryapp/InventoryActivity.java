27dd4b0f0495b8ab0be96d7a225903dce041bde6
shaefferm <matt@theshaeffers.com>
2016-11-22 22:57:08 -0600

    list_item added

--- app/src/main/java/com/theshaeffers/inventoryapp/InventoryActivity.java
+++ app/src/main/java/com/theshaeffers/inventoryapp/InventoryActivity.java
@@ -1,7 +1,9 @@
 package com.theshaeffers.inventoryapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.FloatingActionButton;
+
 
 public class InventoryActivity extends AppCompatActivity {
 
