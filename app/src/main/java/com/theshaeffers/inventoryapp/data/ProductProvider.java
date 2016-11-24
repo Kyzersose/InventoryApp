60f7ad0e36f015838dc73c5787299d63254eb560
shaefferm <matt@theshaeffers.com>
2016-11-24 15:32:09 -0600

    ProductProvider added

---
+++ app/src/main/java/com/theshaeffers/inventoryapp/data/ProductProvider.java
@@ -0,0 +1,89 @@
+package com.theshaeffers.inventoryapp.data;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by shaefferm on 11/24/2016.
+ */
+
+public final class ProductProvider {
+
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    private ProductProvider() {}
+
+    //CONTENT_AUTHORITY used for the content provider
+    public static final String CONTENT_AUTHORITY = "com.theshaeffers.inventoryapp";
+
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.theshaeffers.inventoryapp/products/ is a valid path for
+     * looking at product data.
+     */
+    public static final String PATH_PRODUCTS = "products";
+
+    /**
+     * Inner class that defines constant values for the pets database table.
+     * Each entry in the table represents a single pet.
+     */
+    public static final class ProductEntry implements BaseColumns {
+
+        /** The content URI to access the product data in the provider */
+        public static final Uri CONTENT_URI =
+                Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PRODUCTS);
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a list of products.
+         */
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" +
+                        CONTENT_AUTHORITY + "/" + PATH_PRODUCTS;
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a single product.
+         */
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" +
+                        CONTENT_AUTHORITY + "/" + PATH_PRODUCTS;
+
+        /** Name of database table for products */
+        public final static String TABLE_NAME = "products";
+
+        /**
+         * Unique ID number for the products (only for use in the database table).
+         *
+         * Type: INTEGER
+         */
+        public final static String _ID = BaseColumns._ID;
+
+        /**
+         * Name of the product.
+         *
+         * Type: TEXT
+         */
+        public final static String COLUMN_PRODUCT_NAME ="name";
+
+        /**
+         * Price of the product.
+         *
+         * Type: INTEGER
+         */
+        public final static String COLUMN_PET_PRICE = "price";
+
+        /**
+         * Quantity of the product.
+         *
+         * Type: INTEGER
+         */
+        public final static String COLUMN_PET_QUANTITY = "quantity";
+    }
+
+}
