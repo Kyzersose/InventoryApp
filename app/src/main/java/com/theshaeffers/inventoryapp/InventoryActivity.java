04a7995f78f71ae298ef62034b72d20aba2a916f
shaefferm <matt@theshaeffers.com>
2016-11-26 21:30:46 -0600

    Menu's added. Work in inventory + dummy data

--- app/src/main/java/com/theshaeffers/inventoryapp/InventoryActivity.java
+++ app/src/main/java/com/theshaeffers/inventoryapp/InventoryActivity.java
@@ -1,8 +1,12 @@
 package com.theshaeffers.inventoryapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.design.widget.FloatingActionButton;
+import android.view.Menu;
+import android.view.View;
 
 
 public class InventoryActivity extends AppCompatActivity {
@@ -11,5 +15,24 @@ public class InventoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inventory_activity);
+
+        // Setup FAB to open EditorActivity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(InventoryActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_editor.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.inventory_menu, menu);
+        return true;
     }
 }
+
