79279cc9e4bf7ea701de49c87f8c3fbe50ea6035
shaefferm <matt@theshaeffers.com>
2016-11-26 19:29:36 -0600

    Provider and DbHelper completed

--- app/src/main/java/com/theshaeffers/inventoryapp/data/ProductProvider.java
+++ app/src/main/java/com/theshaeffers/inventoryapp/data/ProductProvider.java
@@ -1,89 +1,169 @@
 package com.theshaeffers.inventoryapp.data;
 
-import android.content.ContentResolver;
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
-import android.provider.BaseColumns;
+import android.util.Log;
+
+import com.theshaeffers.inventoryapp.data.ProductContract.ProductEntry;
 
 /**
  * Created by shaefferm on 11/24/2016.
  */
 
-public final class ProductProvider {
+public final class ProductProvider extends ContentProvider {
 
-    // To prevent someone from accidentally instantiating the contract class,
-    // give it an empty constructor.
-    private ProductProvider() {}
+    /** Tag for the log messages */
+    public static final String LOG_TAG = ProductProvider.class.getSimpleName();
 
-    //CONTENT_AUTHORITY used for the content provider
-    public static final String CONTENT_AUTHORITY = "com.theshaeffers.inventoryapp";
+    /** URI matcher code for the content URI for the products table */
+    private static final int PRODUCTS = 100;
 
-    /**
-     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
-     * the content provider.
-     */
-    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    /** URI matcher code for the content URI for a single product in the products table */
+    private static final int PRODUCTS_ID = 101;
 
     /**
-     * Possible path (appended to base content URI for possible URI's)
-     * For instance, content://com.theshaeffers.inventoryapp/products/ is a valid path for
-     * looking at product data.
+     * UriMatcher object to match a content URI to a corresponding code.
+     * The input passed into the constructor represents the code to return for the root URI.
+     * It's common to use NO_MATCH as the input for this case.
      */
-    public static final String PATH_PRODUCTS = "products";
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
 
-    /**
-     * Inner class that defines constant values for the pets database table.
-     * Each entry in the table represents a single pet.
-     */
-    public static final class ProductEntry implements BaseColumns {
+    // Static initializer. This is run the first time anything is called from this class.
+    static {
+        // The calls to addURI() go here, for all of the content URI patterns that the provider
+        // should recognize. All paths added to the UriMatcher have a corresponding code to return
+        // when a match is found.
 
-        /** The content URI to access the product data in the provider */
-        public static final Uri CONTENT_URI =
-                Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PRODUCTS);
+        // The content URI of the form "content://com.theshaeffers.inventoryapp/products" will
+        // map to the integer code {@link #PRODUCTS}. This URI is used to provide access to MULTIPLE
+        // rows of the products table.
+        sUriMatcher.addURI(ProductContract.CONTENT_AUTHORITY, ProductContract.PATH_PRODUCTS,
+                PRODUCTS);
 
-        /**
-         * The MIME type of the {@link #CONTENT_URI} for a list of products.
-         */
-        public static final String CONTENT_LIST_TYPE =
-                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" +
-                        CONTENT_AUTHORITY + "/" + PATH_PRODUCTS;
+        // The content URI of the form "content://com.theshaeffers.inventoryapp/products/#"
+        // will map to the integer code {@link #PET_ID}. This URI is used to provide access to
+        // ONE single row of the products table.
+        //
+        // In this case, the "#" wildcard is used where "#" can be substituted for an integer.
+        // For example, "content://com.theshaeffers.inventoryapp/products/3" matches, but
+        // "content://com.theshaeffers.inventoryapp/products/" (without a number at the end)
+        // doesn't match.
+        sUriMatcher.addURI(ProductContract.CONTENT_AUTHORITY, ProductContract.PATH_PRODUCTS +
+                "/#", PRODUCTS_ID);
+    }
 
-        /**
-         * The MIME type of the {@link #CONTENT_URI} for a single product.
-         */
-        public static final String CONTENT_ITEM_TYPE =
-                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" +
-                        CONTENT_AUTHORITY + "/" + PATH_PRODUCTS;
+    /** Database helper object */
+    private PetDbHelper mDbHelper;
 
-        /** Name of database table for products */
-        public final static String TABLE_NAME = "products";
+    @Override
+    public boolean onCreate() {
+        mDbHelper = new PetDbHelper(getContext());
+        return true;
+    }
 
-        /**
-         * Unique ID number for the products (only for use in the database table).
-         *
-         * Type: INTEGER
-         */
-        public final static String _ID = BaseColumns._ID;
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+                        String sortOrder) {
+        // Get readable database
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
 
-        /**
-         * Name of the product.
-         *
-         * Type: TEXT
-         */
-        public final static String COLUMN_PRODUCT_NAME ="name";
+        // This cursor will hold the result of the query
+        Cursor cursor;
 
-        /**
-         * Price of the product.
-         *
-         * Type: INTEGER
-         */
-        public final static String COLUMN_PET_PRICE = "price";
+        // Figure out if the URI matcher can match the URI to a specific code
+        int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PRODUCTS:
+                // For the PRODUCTS code, query the products table directly with the given
+                // projection, selection, selection arguments, and sort order. The cursor
+                // could contain multiple rows of the products table.
+                cursor = database.query(ProductEntry.TABLE_NAME, projection, selection,
+                        selectionArgs, null, null, sortOrder);
+                break;
+            case PRODUCTS_ID:
+                // For the PRODUCTS_ID code, extract out the ID from the URI.
+                // For an example URI such as "content://com.theshaeffers.inventoryapp/products/3",
+                // the selection will be "_id=?" and the selection argument will be a
+                // String array containing the actual ID of 3 in this case.
+                //
+                // For every "?" in the selection, we need to have an element in the selection
+                // arguments that will fill in the "?". Since we have 1 question mark in the
+                // selection, we have 1 String in the selection arguments' String array.
+                selection = ProductEntry._ID + "=?";
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
 
-        /**
-         * Quantity of the product.
-         *
-         * Type: INTEGER
-         */
-        public final static String COLUMN_PET_QUANTITY = "quantity";
+                // This will perform a query on the pets table where the _id equals 3 to return a
+                // Cursor containing that row of the table.
+                cursor = database.query(ProductEntry.TABLE_NAME, projection, selection,
+                        selectionArgs, null, null, sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
+        }
+
+        // Set notification URI on the Cursor,
+        // so we know what content URI the Cursor was created for.
+        // If the data at this URI changes, then we know we need to update the Cursor.
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+        // Return the cursor
+        return cursor;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PRODUCTS:
+                return insertProduct(uri, contentValues);
+            default:
+                throw new IllegalArgumentException("Insertion is not supported for " + uri);
+        }
+    }
+
+    private Uri insertProduct(Uri uri, ContentValues values) {
+        // Check that the name is not null
+        String name = values.getAsString(ProductEntry.COLUMN_PRODUCT_NAME);
+        if (name == null) {
+            throw new IllegalArgumentException("Product requires a name");
+        }
+
+        // Check that the price is valid
+        //TODO check to make sure the input is *.00
+        Integer price = values.getAsInteger(ProductEntry.COLUMN_PRODUCT_PRICE);
+        if (price == null || !PetEntry.isValidGender(gender)) {
+            throw new IllegalArgumentException("Pet requires valid gender");
+        }
+
+        // If the weight is provided, check that it's greater than or equal to 0 kg
+        Integer weight = values.getAsInteger(PetEntry.COLUMN_PET_WEIGHT);
+        if (weight != null && weight < 0) {
+            throw new IllegalArgumentException("Pet requires valid weight");
+        }
+
+        // No need to check the breed, any value is valid (including null).
+
+        // Get writeable database
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+
+        // Insert the new pet with the given values
+        long id = database.insert(PetEntry.TABLE_NAME, null, values);
+        // If the ID is -1, then the insertion failed. Log an error and return null.
+        if (id == -1) {
+            Log.e(LOG_TAG, "Failed to insert row for " + uri);
+            return null;
+        }
+
+        // Notify all listeners that the data has changed for the pet content URI
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        // Return the new URI with the ID (of the newly inserted row) appended at the end
+        return ContentUris.withAppendedId(uri, id);
     }
 
 }
